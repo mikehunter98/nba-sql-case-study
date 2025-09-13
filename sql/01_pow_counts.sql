52bf531b4aec90b33a886456a2ba57b4683c7b82
Michael Hunter <mikehunte2006@gmail.com>
2025-09-13 11:35:01 +0800

    Added proper 01_pow_counts.sql

--- sql/01_pow_counts.sql
+++ sql/01_pow_counts.sql
@@ -1,1 +1,9 @@
--- 
+-- 01_pow_counts.sql
+-- Counts number of Player of the Week awards by player
+
+SELECT
+  Player,
+  COUNT(*) AS pow_awards
+FROM `nba-sql-case-study.nba_awards.player_of_week`
+GROUP BY Player
+ORDER BY pow_awards DESC;
